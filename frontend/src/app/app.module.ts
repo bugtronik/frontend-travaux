a4548409c95f49db833a61bf37bbf1c795486fce
BugTekNik <bugtronik@gmail.com>
2023-11-09 12:42:55 +0100

    Ajout de la config datatables au projet

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { DataTablesModule } from 'angular-datatables';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DataTablesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
